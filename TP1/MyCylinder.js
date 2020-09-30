c18d64022b457415084115e7c91abc66c5696417
João Martins <up201806436@fe.up.pt>
2020-09-30 18:28:16 +0100

    Fix ciloinders vertices. Added texture coords too, but texture in base isn't working yet.

--- TP1/MyCylinder.js
+++ TP1/MyCylinder.js
@@ -1,40 +1,56 @@
 class MyCylinder extends CGFobject {
     constructor(scene, bottomRadius, topRadius, height, slices, stacks) {
         super(scene);
-        this.radius = bottomRadius; //TODO
         this.height = height;
         this.slices = slices;
         this.stacks = stacks;
+        this.bottomRadius = bottomRadius;
+        this.topRadius = topRadius;
 
         this.initBuffers();
     }
 
     initBuffers() {
+        /* Initialization */
         this.vertices = [];
         this.indices = [];
         this.normals = [];
-        this.texCoords = []; // TODO tex coords
-        // TODO Clean dis
-
+        this.texCoords = []; // TODO tex coords of bases
+        // Aux Variables
+        var radius = this.bottomRadius; var radiusDelta = (this.topRadius - this.bottomRadius) / this.stacks;
         var heightDelta = this.height / this.stacks;
         var rotationDelta = (2 * Math.PI) / this.slices;
-        var z = 0; var c = 0;
-        for (var i = 0; i < this.stacks + 1; ++i) {
-            var theta = 0;
-            for (var j = 0; j < this.slices; ++j) {
-                this.vertices.push(Math.cos(theta), Math.sin(theta), z)
-                this.normals.push(Math.cos(theta), Math.sin(theta), z)
-                console.log(c++);
-                theta += rotationDelta;
-            }
-            z += heightDelta;
-        }
-        var bottom_vertex = (this.slices - 1) * this.stacks;
-        this.vertices.push(0, 0, 0); this.normals.push(0, 0, -1); // Bottom Vertex
-        var top_vertex = (this.slices - 1) * this.stacks + 1;
-        this.vertices.push(0, 0, this.height); this.normals.push(0, 0, 1); // Top Vertex
+        var z = 0, theta = 0; var v = 0;
+        var texStepX = 1 / this.slices; var texStepY = 1 / (this.stacks + 1);
+        var texCurrX = 0; var texCurrY = 0;
 
-        // Face
+        /* Set vertices */
+        for (var i = 0; i < this.stacks + 1; ++i) {
+            theta = 0; texCurrX = 0;
+            for (var j = 0; j < this.slices; ++j) {
+                this.vertices.push(Math.cos(theta) * radius, Math.sin(theta) * radius, z)
+                this.normals.push(Math.cos(theta) * radius, Math.sin(theta) * radius, z)
+                theta += rotationDelta;
+                console.log(v++);
+
+                //console.log(texCurrY + " " + texCurrX);
+                this.texCoords.push(texCurrX, texCurrY);
+                texCurrX += texStepX;
+            }
+            console.log("-");
+            z += heightDelta;
+            radius += radiusDelta;
+            texCurrY += texStepY;
+        }
+        console.log("-----");
+        var bottom_vertex = (this.slices) * (this.stacks + 1);
+        this.vertices.push(0, 0, 0); this.normals.push(0, 0, -1); // Bottom Vertex
+        this.texCoords.push(0, 0); // FIXME texcoords in base
+        var top_vertex = bottom_vertex + 1;
+        this.vertices.push(0, 0, this.height); this.normals.push(0, 0, 1); // Top Vertex
+        this.texCoords.push(1, 1);
+
+        /* Push Indeces */
         for (var i = 0; i < this.stacks; ++i) {
             for (var j = 0; j < this.slices - 1; ++j) {
                 var one = j + i * this.slices;
@@ -57,6 +73,7 @@ class MyCylinder extends CGFobject {
             var two = (j + 1) % this.slices;
             var new_one = one + this.slices * (this.stacks);
             var new_two = two + this.slices * (this.stacks);
+            console.log(one, two, new_one, new_two, bottom_vertex, top_vertex);
             this.indices.push(two, one, bottom_vertex);
             this.indices.push(new_one, new_two, top_vertex);
         }
--- TP1/MySceneGraph.js
+++ TP1/MySceneGraph.js
@@ -49,6 +49,16 @@ class MySceneGraph {
          * If any error occurs, the reader calls onXMLError on this object, with an error message
          */
         this.reader.open('scenes/' + filename, this);
+
+        // TODO remove - testing
+        this.ciloinders = new MyCylinder(this.scene, 10, 10, 10, 3, 3);
+        this.quadMaterial = new CGFappearance(this.scene);
+        this.quadMaterial.setAmbient(1.1, 0.1, 0.1, 1);
+        this.quadMaterial.setDiffuse(10.9, 0.9, 0.9, 1);
+        this.quadMaterial.setSpecular(10.1, 0.1, 0.1, 1);
+        this.quadMaterial.setShininess(10.0);
+        this.quadMaterial.loadTexture('scenes/images/rocks.jpg');
+        this.quadMaterial.setTextureWrap('REPEAT', 'REPEAT');
     }
 
     /*
@@ -686,7 +696,9 @@ class MySceneGraph {
      */
     displayScene() {
         //To do: Create display loop for transversing the scene graph, calling the root node's display function
-        this.rootNode.display();
+        // this.rootNode.display();
+        this.quadMaterial.apply();
+        this.ciloinders.display();
         //this.nodes[this.idRoot].display()
     }
 }
