013367b6c529b23272b924cca80450014fe131b0
João Martins <up201806436@fe.up.pt>
2020-10-17 22:10:22 +0100

    Bandtape fix to triangular prism's texture problem

--- TP1/MyTriangle.js
+++ TP1/MyTriangle.js
@@ -13,8 +13,8 @@ class MyTriangle extends CGFobject {
     constructor(scene, x1, y1, x2, y2, x3, y3, afs = 1.0, aft = 1.0) {
         super(scene);
         this.x1 = x1;
-        this.x2 = x2;
         this.y1 = y1;
+        this.x2 = x2;
         this.y2 = y2;
         this.x3 = x3;
         this.y3 = y3;
@@ -63,7 +63,7 @@ class MyTriangle extends CGFobject {
         this.texCoords = [
             0, 0,
             a / this.afs, 0,
-            c * cosa / this.aft, c * sina / this.afs
+            c * cosa / this.afs, c * sina / this.aft
         ]
         this.primitiveType = this.scene.gl.TRIANGLES;
         this.initGLBuffers();
