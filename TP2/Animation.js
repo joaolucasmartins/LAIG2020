7790e3aa44046783b527f2d1c8aa9626db68c03a
Ivo Saavedra <ivo_saavedra@outlook.com>
2020-11-03 10:48:06 +0000

    started spriteSheet

---
+++ TP2/Animation.js
@@ -0,0 +1,15 @@
+class Animation {
+     /**
+     * @constructor
+     * @param {inst} animation instants
+     * @param {transfMat} transformations matrix
+     */
+    constructor(inst, transfMat) {
+        super();
+
+        this.inst = inst;
+        this.transfMat = transfMat;
+    }
+
+
+}
---
+++ TP2/MySpriteSheet.js
@@ -0,0 +1,8 @@
+class MySpriteSheet {
+    constructor(texture, sizeM, sizeN) {
+        this.texture = texture;
+        this.sizeM = sizeM;
+        this.sizeN = sizeN;
+    }
+
+}
