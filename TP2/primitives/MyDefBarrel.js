165791cdcd1369d277e903aaa3e486a072371724
João Martins <up201806436@fe.up.pt>
2020-11-21 00:16:28 +0000

    Commented ref barrel

--- TP2/primitives/MyDefBarrel.js
+++ TP2/primitives/MyDefBarrel.js
@@ -2,15 +2,19 @@
  * MyDefBarrel
  * @constructor
  * @param {CGFscene} scene - Reference to MyScene object
- * @param {integer} u  - number of u divisions
- * @param {integer} v - number of v divisions
+ * @param {float} base  - radius of the base
+ * @param {float} middle - radius of the middle of the barrel
+ * @param {float} height - height of the barrel
+ * @param {integer} slices - number of cuts in X
+ * @param {integer} stacks - number of cuts in Y
+ * @param {float} tilt - angle between Q1Q2 and Q1Q4. Defaults to 20
  */
 class MyDefBarrel extends CGFobject {
     static ALPHA = 20; // Default value of tilt if not given
     constructor(scene, base, middle, height, slices, stacks, tilt = MyDefBarrel.ALPHA) {
         super(scene);
 
-        // Moved in x by r/bottom to be rotated in Z later
+        // max and min ensure that middle Q2 and Q3 don't surpass the middle of the barrel
         const H = (4 / 3) * (middle - base);
         const baseQ = [
             [0, 0, 0],
@@ -22,22 +26,22 @@ class MyDefBarrel extends CGFobject {
         const h = 4 * base / 3;
         const PList =
             [
-                [-base, 0, 0, 1],
-                [-base, h, 0, 1],
-                [base, h, 0, 1],
-                [base, 0, 0, 1]
+                [-base, 0, 0], // P1
+                [-base, h, 0], // P2
+                [base, h, 0],  // P3
+                [base, 0, 0]   // P4
             ];
 
         let controlPoints = [];
-        for (let j = 0; j < 4; ++j) {
+        for (let j = 0; j < 4; ++j) { // For each P
             let currentP = PList[j];
             let currentPx = currentP[0], currentPy = currentP[1];
             let signal = 1;
-            if (currentPx < 0)
+            if (currentPx < 0) // If P1 or P2, all increments must be done -x wise
                 signal = -1;
 
             let res = [];
-            for (let k = 3; k >= 0; --k) {
+            for (let k = 3; k >= 0; --k) { // For each Q 
                 let Q = [...baseQ[k]];
                 Q[0] = currentPx + Q[0] * signal;
                 Q[1] = currentPy + Q[1] * signal;
@@ -48,14 +52,17 @@ class MyDefBarrel extends CGFobject {
             controlPoints.push(res);
         }
 
+        // nr and nh are to the Q points the equivalent of r and h to the P points.
+        // We got these measures by creating a convex hull that passes through the middle
+        // of the barrel (R).
         const nr = base + H;
         const nh = (4 / 3) * nr;
         for (let j = 0; j < 2; ++j) { // Iterate trough Q2 and Q3
             let P2_Points = controlPoints[1];
             let P3_Points = controlPoints[2];
-            P2_Points[1 + j][0] = - nr;
+            //P2_Points[1 + j][0] = - nr; Not necessary as it is already done when previously incrementing H
+            //P3_Points[1 + j][0] = nr;   Still left it here for better readability
             P2_Points[1 + j][1] = nh;
-            P3_Points[1 + j][0] = nr;
             P3_Points[1 + j][1] = nh;
         }
 
@@ -66,7 +73,7 @@ class MyDefBarrel extends CGFobject {
     display() {
         this.obj.display();
         this.scene.pushMatrix();
-        this.scene.rotate(180, 1, 0, 0);
+        this.scene.rotate(Math.PI, 0, 0, 1);
         this.obj.display();
         this.scene.popMatrix();
     }
